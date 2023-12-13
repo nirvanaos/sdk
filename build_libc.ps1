aab809a8aadbddb24b10b8ff0d21b76bd146a8ff
Igor Popov <popov.nirvana@gmail.com>
2023-12-13 23:02:41 +0300

    + llvm-project

--- build_libc.ps1
+++ build_libc.ps1
@@ -1,4 +1,4 @@
-$llvm_root = "C:\0work\Nirvana\Work\llvm-project"
+$llvm_root = $PSScriptRoot + "\llvm-project"
 $libc_config = $PSScriptRoot + "\libc"
 
 cd $llvm_root
@@ -6,7 +6,7 @@ cd $llvm_root
 mkdir .\build
 cd .\build
 
-& cmake ../llvm -G Ninja -DLLVM_ENABLE_PROJECTS="libc" -DLIBC_CONFIG_PATH="$libc_config" `
+& cmake ../llvm -G Ninja -DLLVM_ENABLE_PROJECTS="libc" -DLIBC_CONFIG_PATH="$libc_config" -DLIBC_TARGET_ARCHITECTURE=x86_64 `
 	-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ `
 	-DCMAKE_BUILD_TYPE=Debug `
 	-DCMAKE_INSTALL_PREFIX="c:\nirvanasdk"
