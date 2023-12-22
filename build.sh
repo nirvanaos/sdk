7f4d2e0e6b8bb7d5627e04993c9df27e2b3e4682
Igor Popov <popov.nirvana@gmail.com>
2023-12-22 06:25:43 +0300

    Fix build

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 set -e
 
 ./build_compiler.sh
-./build_libraries.sh
+
+./build_libraries.sh debug
+./build_libraries.sh release
 
 ./build_libc.sh x64 Debug
 ./build_libc.sh x64 Release
--- build_libraries.sh
+++ build_libraries.sh
@@ -1,9 +1,15 @@
 dest_dir="$PWD/distr"
 build_dir="build"
 
+if [[ $# -ge 1 ]]; then
+	config=$1
+else
+	config="debug"
+fi
+
 set -e
 
-meson setup $build_dir --reconfigure
+meson setup $build_dir --reconfigure --buildtype $config
 #meson compile -C $build_dir
 meson test -C $build_dir
 meson install -C $build_dir --destdir $dest_dir
