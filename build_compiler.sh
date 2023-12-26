ea4c0b887ec58dd1869dd9330824a626a1c3f795
Igor Popov <popov.nirvana@gmail.com>
2023-12-26 13:19:22 +0300

    Fixes

--- build_compiler.sh
+++ build_compiler.sh
@@ -7,5 +7,5 @@ host_system="windows"
 fi
 
 cmake -B $build_dir -S nidl2cpp
-cmake --build $build_dir --config Debug
-cmake --install $build_dir --config Debug --prefix $dest_dir/$host_system
+cmake --build $build_dir --config Release
+cmake --install $build_dir --config Release --prefix $dest_dir/$host_system
--- build_libc.sh
+++ build_libc.sh
@@ -13,9 +13,9 @@ fi
 set -e
 
 dest_dir="$PWD/sdk"
-build_dir="$PWD/build-libc-$platform-$config"
-main_build="$PWD/build"
-llvm_root="$PWD/llvm-project"
+build_dir="build-libc-$platform-$config"
+main_build="build"
+llvm_root="llvm-project"
 libc_config="$PWD/libc"
 
 #if [[ "$platform" == "x64" ]]; then
@@ -31,23 +31,28 @@ triple="i686-w64-windows-gnu"
 cpu_features="MMX;SSE"
 fi
 
-win_path=$(pwd -W)
+#meson install -C $main_build --tags inc --destdir $build_dir/projects/libc
+nirvana="$(pwd -W)/nirvana"
 
 cpp_flags="-DLIBC_COPT_USE_C_ASSERT -D_LIBCPP_PROVIDES_DEFAULT_RUNE_TABLE -D_LIBCPP_DISABLE_VISIBILITY_ANNOTATIONS\
  -D_LIBCPP_OBJECT_FORMAT_COFF -fshort-wchar\
  -Wno-unused-function -Wno-implicit-int-conversion -Wno-implicit-int-float-conversion -Wno-c99-extensions -Wno-all\
- -U_WIN32 -U__MINGW32__ -U__MINGW64__ -nostdinc -nostdinc++ -I$win_path/nirvana/library/Include -I$win_path/nirvana/orb/Include\
- -IC:/msys64/clang64/include/c++/v1 -IC:/msys64/clang64/lib/clang/17/include -I$win_path/nirvana/library/Include/CRTL"
+ -U_WIN32 -U__MINGW32__ -U__MINGW64__ -nostdinc -nostdinc++\
+ -IC:/msys64/clang64/include/c++/v1 -IC:/msys64/clang64/lib/clang/17/include -I$nirvana/library/Include/CRTL -I$nirvana/library/Include"
 
 # echo $cpp_flags
 
 #	-DLLVM_ENABLE_LIBCXX=ON \
-#	-DLIBC_COMPILE_OPTIONS_DEFAULT="$cpp_flags" \
+#	
 #	-DCMAKE_C_FLAGS="$cpp_flags -std=c11" \
 #	-DLIBC_COMMON_TUNE_OPTIONS="-Wno-unused-function -Wno-implicit-int-conversion -Wno-implicit-int-float-conversion -Wno-c99-extensions -Wno-all" \
+# LIBC_COMPILE_OPTIONS_DEFAULT
 cmake "$llvm_root/llvm" -B "$build_dir" -G Ninja -DLLVM_ENABLE_PROJECTS="libc" -DLIBC_CONFIG_PATH="$libc_config" \
 	-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ \
 	-DCMAKE_CXX_FLAGS="$cpp_flags" \
+	-DLLVM_ENABLE_LIBCXX=ON \
+	-DHAVE_CXX_ATOMICS_WITHOUT_LIB=ON \
+	-DHAVE_CXX_ATOMICS64_WITHOUT_LIB=ON \
 	-DLIBC_TARGET_TRIPLE="$triple" \
 	-DLIBC_CPU_FEATURES="$cpu_features" \
 	-DCMAKE_BUILD_TYPE="$config"
